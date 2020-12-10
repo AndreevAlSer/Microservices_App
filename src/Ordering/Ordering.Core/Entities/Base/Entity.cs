69ad14ec54e580aa7ef27e0993fe7785c5a0b1e0
Oleksandr Andreiev <andreevalser17@gmail.com>
2020-12-10 22:19:26 +0200

    OrderContextSeed Method For ordering infrastructure was done

--- src/Ordering/Ordering.Core/Entities/Base/Entity.cs
+++ src/Ordering/Ordering.Core/Entities/Base/Entity.cs
@@ -8,5 +8,6 @@ namespace Ordering.Core.Entities.Base
 {
     public abstract class Entity : EntityBase<int>
     {
+
     }
 }
--- src/Ordering/Ordering.Core/Entities/Order.cs
+++ src/Ordering/Ordering.Core/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ordering.Core.Entities.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace Ordering.Core.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
         public string Country { get; set; }
         public string State { get; set; }
         public string ZipCode { get; set; }
